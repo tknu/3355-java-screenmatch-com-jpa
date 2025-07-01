0e06244d1266d86fff3a0103e9b24e6809a76b2c
Thiago Ueda <thiagokoiti@gmail.com>
2025-07-01 07:46:46 -0300

    Ajustando a chave estrangeira

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -27,7 +27,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
     
     public Serie() {}
@@ -57,6 +57,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -124,6 +125,7 @@ public class Serie {
                 ", avaliacao=" + avaliacao +
                 ", atores='" + atores + '\'' +
                 ", poster='" + poster + '\'' +
-                ", sinopse='" + sinopse + '\'';
+                ", sinopse='" + sinopse + '\''+
+                ", episodios='" + episodios + '\'';
     }
 }
