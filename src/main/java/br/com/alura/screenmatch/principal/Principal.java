bfaad965e7ea682ca9ce813ebba22b9199cc875d
Thiago Ueda <thiagokoiti@gmail.com>
2025-07-02 06:20:02 -0300

    buscas com múltiplos critérios

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -38,6 +38,7 @@ public class Principal {
                     2 - Buscar episódios
                     3 - Listar séries buscadas
                     4 - Buscar série por título
+                    5 - Buscar série por ator
                                     
                     0 - Sair                                 
                     """;
@@ -59,6 +60,9 @@ public class Principal {
                 case 4:
                     buscarSeriePorTitulo();
                     break;
+                case 5:
+                    buscarSeriePorAtor();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -67,6 +71,7 @@ public class Principal {
             }
         }
     }
+
 
 
 
@@ -142,5 +147,16 @@ public class Principal {
         } else {
             System.out.println("Série não encontrada!");
         }
+    }
+
+    private void buscarSeriePorAtor() {
+        System.out.println("Qual o nome do ator/ atriz para a busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de qual valor?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanequal(nomeAtor, avaliacao);
+        System.out.println("Séries em que que " + nomeAtor + " trabalhou: ");
+        seriesEncontradas.forEach(s ->
+                System.out.println(s.getTitulo() + " Avaliação: " + s.getAvaliacao()));
     }
 }
